b6411edd7b966cbc15ef5ac55aef4f38869341ce
guojun <guojun6@tal.com>
2022-08-30 17:08:14 +0800

    fix Foreach UnMarshal

--- example/xslice_test0.go
+++ example/xslice_test0.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-//	"github.com/heiyeluren/xmm"
+	//	"github.com/heiyeluren/xmm"
 	"github.com/heiyeluren/xds"
 	"github.com/heiyeluren/xds/xslice"
 )
@@ -16,7 +16,7 @@ func main() {
 	//创建XSlice对象, 类似于 s0 []Int64 = make([]Int64, 1)
 	s0 := xslice.NewXslice(xds.Int,1)
 
-	//Set压入数据, 类似于 s0 :=[] int { 11, 22, 33 } 
+	//Set压入数据, 类似于 s0 :=[] int { 11, 22, 33 }
 	s0.Set(1, 11)
 	s0.Set(2, 22)
 	s0.Set(3, 33)
@@ -36,40 +36,20 @@ func main() {
 	s0.Free()
 
 	//批量压入Int ( 类似于 s1 []Int64 = make([]Int64, 1) )
-	s1 := xslice.NewXslice(xds.Int, 10)
+	s1 := xslice.NewXslice(xds.Int, 1)
 	for i:=50; i<=55; i++ {
 		s1.Append(i)
 	}
+
+	//fmt.Println(s1.Get(2))
+
 	//使用ForEach读取所有数据
-	s1.ForEach(func(i1 int, v1 []byte) error {
-		fmt.Println("XSlice foreach data i: ",i1, " v: ", string(v1))
+	s1.ForEach(func(i int, v interface{}) error {
+		fmt.Println("XSlice foreach data i: ",i, " v: ", v)
 		return nil
 	})
-// 	fmt.Println(s1.s)
+	//fmt.Println(s1)
 
-	//读取整个slice长度, 类似于 len()
+	////读取整个slice长度, 类似于 len()
 	fmt.Println("XSlice data size: ", s1.Len(), "\n")
-
-	//使用 Append压入一批String数据 ( 类似于 s2 []String = make([]String, 1) )
-	s2 := xslice.NewXslice(xds.String, 10)
-	s2.Append("aaa")
-	s2.Append("bbb")
-	s2.Append("ccc")
-	s2.Append("ddd")
-	s2.Append("eee")
-
-	//使用ForEach读取所有数据
-	s2.ForEach(func(i2 int, v2 []byte) error {
-		fmt.Println("XSlice foreach data i: ", i2, " v: ", string(v2))
-		return nil
-	})
-
-	//读取整个slice长度, 类似于 len()
-	fmt.Println("XSlice data size: ", s2.Len(), "\n")
-
-	//释放资源
-	s2.Free()
-
-
-
 }
